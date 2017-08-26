c56dc2a88889db123c2091d8a62dc440920cac39
Damian Szewczyk <damian.szewczyk@fastwhitecat.com>
2017-08-26 14:55:24 +0200

    removed js

--- resources/views/includes/foot.blade.php
+++
@@ -1,9 +0,0 @@
-<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js"></script>
-
-<script type="text/javascript">
-    $(document).ready(function() {
-        $('#summernote').summernote({
-            height:300,
-        });
-    });
-</script>

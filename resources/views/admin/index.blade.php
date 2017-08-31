0e2855391fb389904448c2f21710f19e97f15907
pawel.jakimowicz <pawel.jakimowicz@fastwhitecat.com>
2017-08-31 08:20:03 +0200

    new admin layout

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+@section('content')
+
+
+            <p>tresc panelu</p>
+
+
+
+
+@endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,26 +1,58 @@
 <html>
 <head>
-    @include('includes.head')
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Food Think Tank</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <link href="{{ asset('css/backend/sb-admin.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/backend/app.css') }}" rel="stylesheet">
+    <script src="https://use.fontawesome.com/931abd4fe3.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 </head>
-<body>
-<div class="container">
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+
+        @include('includes.backend.sidebar')
+
+        <div class="content-wrapper">
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+        </div>
+    </div>
     <div class="row">
-        @yield('admin')
+        <!-- Scroll to Top Button -->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fa fa-angle-up"></i>
+        </a>
+
+        <!-- Logout Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Select "Logout" below if you are ready to end your current session.
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <a class="btn btn-primary" href="login.html">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <header class="row">
-
-    </header>
-    <div id="main" class="row">
-
-        @yield('content')
-
-
-    </div>
-
-    <footer class="row">
-
-    </footer>
-
 </div>
+<script src="{{ asset('js/backend/sb-admin.js') }}"></script>
 </body>
 </html>
